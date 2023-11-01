37c5b84d2c776327ebd2fef6360f8ee08348540e
Nery Rodriguez <neryrod@gmail.com>
2023-11-01 06:46:27 -0600

    Tarea2 actualización Nery Rodriguez

--- Tarea_2/Nery Rodriguez/Tarea2.dart
+++ Tarea_2/Nery Rodriguez/Tarea2.dart
@@ -1,36 +1,15 @@
-void main() {
- 
-  final Map<String, dynamic>carro ={
-    'marca':'Toyota',
-    'motor': true,
-    'klm': 4000,
-    'detalle':<String>['1','1'],
-    'submapa': <int, String>{
-      1: 'ABC',
-      2: 'CDF',
-      3: 'FRE',
-    }
-  };
-  
-  final carros = {
-    1: 'ABC',
-    2: 'CDF',
-    3: 'FRE',
-  };
-  
-  //print(carro);
-  //print(carros);
-  
-  //print('Hola ${carro['marca']}');
-  //print('Hola ${carro[1]}');
-  // print('Hola ${carros[2]}');
-  //print('Submapa 2 ${carro.values.elementAt(4)}');//Muestro todo el contenido de submapa
-  
-  //Tarea 2:
-  //Selecciona categoría 2 de 'submapa'
-  //Fuente: https://www.youtube.com/watch?v=ps_xpkruXbw
-  //Titulo: Flutter Widget Column, Spread Operator, List, and Map All in One
-  //Canal: dbestech
-  print('Submapa 2: ${carro.values.elementAt(4)[2]}'); //Según ejemplo 2 >carro
-  
+//Tarea 2
+
+//Ejercicios 
+
+//1) Convertir funcion de sumar int en Lambda function/funcion de flecha:
+void main (){
+  print (addNumbers(8,5));
 }
+
+//int addNumbers (int a, int b) {
+//  return a+b;
+//}
+
+
+  final addNumbers = (int a, int b) => a+ b;
